4dc32aa70a2647351a758752ba2ac885b2cb0489
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-30 12:16:00 -0500

    gdfgxfd

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
-
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+document.addEventListener(“DOMContentLoaded”, () => {
+  // console.log(“The DOM content has loaded!“)
+  const button = document.querySelector(‘#save_lincoln’)
+  const interval = document.querySelector(‘#interval’)
+  const parsedInterval = parseInt(interval.value)
+  let fadeTarget = document.querySelector(‘#foreground’)
+  let lincoln = document.querySelector(‘#background’)
+ 
+  const callback = function() {
+    fadeTarget.src = lincoln.src;
+    fadeIn(fadeTarget);
   }
-  $('#foreground').fadeOut(interval);
-}
+
+  button.addEventListener(‘click’, function(event) {
+      event.preventDefault()
+      // console.log(“In Function save Lincoln”)
+      var opacity = 1;
+      var timer = setInterval(function(){
+        if(opacity < 0.1){
+          clearInterval(timer);
+          callback(); //this executes the callback function!
+        }
+        fadeTarget.style.opacity = opacity;
+        opacity -=  0.1;
+      }, parsedInterval);
+  }
+
+}) 
