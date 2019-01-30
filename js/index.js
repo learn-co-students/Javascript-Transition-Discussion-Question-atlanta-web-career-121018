38ca1db667e5e2fff84e44c27660c0a079986f73
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-30 12:21:54 -0500

    grgrtsgrsg

--- js/index.js
+++ js/index.js
@@ -1,28 +1,26 @@
-document.addEventListener(“DOMContentLoaded”, () => {
-  // console.log(“The DOM content has loaded!“)
-  const button = document.querySelector(‘#save_lincoln’)
-  const interval = document.querySelector(‘#interval’)
-  const parsedInterval = parseInt(interval.value)
-  let fadeTarget = document.querySelector(‘#foreground’)
-  let lincoln = document.querySelector(‘#background’)
+// document.addEventListener('DOMContentLoaded', function() {
+//   const button = document.querySelector(‘#save_lincoln’)
+//   const interval = document.querySelector(‘#interval’)
+//   const parsedInterval = parseInt(interval.value)
+//   let fadeTarget = document.querySelector(‘#foreground’)
+//   let lincoln = document.querySelector(‘#background’)
  
-  const callback = function() {
-    fadeTarget.src = lincoln.src;
-    fadeIn(fadeTarget);
-  }
+//   const callback = function() {
+//     fadeTarget.src = lincoln.src;
+//     fadeIn(fadeTarget);
+//   }
 
-  button.addEventListener(‘click’, function(event) {
-      event.preventDefault()
-      // console.log(“In Function save Lincoln”)
-      var opacity = 1;
-      var timer = setInterval(function(){
-        if(opacity < 0.1){
-          clearInterval(timer);
-          callback(); //this executes the callback function!
-        }
-        fadeTarget.style.opacity = opacity;
-        opacity -=  0.1;
-      }, parsedInterval);
-  }
+//   button.addEventListener(‘click’, function(event) {
+//       event.preventDefault()
+//       var opacity = 1;
+//       var timer = setInterval(function(){
+//         if(opacity < 0.1){
+//           clearInterval(timer);
+//           callback();
+//         }
+//         fadeTarget.style.opacity = opacity;
+//         opacity -=  0.1;
+//       }, parsedInterval);
+//   }
 
-}) 
+// }) 
